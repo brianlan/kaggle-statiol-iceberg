11039190dfcbbcab61d0e73325ead45ec2c065d4
brianlan <brianlanbo@gmail.com>
2018-01-13 23:05:18 +0800

    implemented ResNet, and use it to generate submissions for the challenge

--- src/network.py
+++ src/network.py
@@ -2,43 +2,23 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class ResBlock(nn.Module):
-    def __init__(self, in_channel):
+class BottleneckBlock(nn.Module):
+    def __init__(self, in_channel, mid_channel, downsample=False, dim_up=False):
         super().__init__()
-        self.conv1 = nn.Conv2d(in_channel, in_channel, 1, padding=1, stride=1)
-        self.conv1_bn = nn.BatchNorm2d(in_channel)
-        self.conv2 = nn.Conv2d(in_channel, in_channel, 3, padding=1, stride=1)
-        self.conv2_bn = nn.BatchNorm2d(in_channel)
-        self.conv3 = nn.Conv2d(in_channel, in_channel * 4, 1, padding=1, stride=1)
-        self.conv3_bn = nn.BatchNorm2d(in_channel * 4)
+        self.downsample = downsample
+        self.dim_up = dim_up
+        self.first_conv_stride = 2 if self.downsample else 1
 
-    # def forward(self, x):
-    #     x = 
+        self.conv1 = nn.Conv2d(in_channel, mid_channel, 1, padding=0, stride=self.first_conv_stride)
+        self.conv1_bn = nn.BatchNorm2d(mid_channel)
+        self.conv2 = nn.Conv2d(mid_channel, mid_channel, 3, padding=1, stride=1)
+        self.conv2_bn = nn.BatchNorm2d(mid_channel)
+        self.conv3 = nn.Conv2d(mid_channel, mid_channel * 4, 1, padding=0, stride=1)
+        self.conv3_bn = nn.BatchNorm2d(mid_channel * 4)
 
-
-class Net(nn.Module):
-    def __init__(self, input_channel=3):
-        super().__init__()
-        self.conv1 = nn.Conv2d(input_channel, 64, 7, padding=1, stride=2)
-        self.conv1_bn = nn.BatchNorm2d(64)
-        self.conv2 = nn.Conv2d(32, 32, 3, padding=1, stride=1)
-        self.conv2_bn = nn.BatchNorm2d(32)
-        self.conv3 = nn.Conv2d(32, 32, 3, padding=1, stride=1)
-        self.conv3_bn = nn.BatchNorm2d(32)
-        self.conv4 = nn.Conv2d(32, 64, 3, padding=1, stride=1)
-        self.conv4_bn = nn.BatchNorm2d(64)
-        self.conv5 = nn.Conv2d(64, 64, 3, padding=1, stride=1)
-        self.conv5_bn = nn.BatchNorm2d(64)
-        self.conv6 = nn.Conv2d(64, 64, 3, padding=1, stride=1)
-        self.conv6_bn = nn.BatchNorm2d(64)
-
-        self.fc1 = nn.Linear(18 * 18 * 64, 120)
-        self.fc1_bn = nn.BatchNorm1d(120)
-        self.fc2 = nn.Linear(120, 80)
-        self.fc2_bn = nn.BatchNorm1d(80)
-        self.fc3 = nn.Linear(80, 2)
-
-        self.initialize_weights()
+        if self.dim_up:
+            self.dim_up_conv = nn.Conv2d(in_channel, mid_channel * 4, 1, padding=0, stride=self.first_conv_stride)
+            self.dim_up_bn = nn.BatchNorm2d(mid_channel * 4)
 
     def initialize_weights(self):
         for m in self.modules():
@@ -46,17 +26,70 @@ class Net(nn.Module):
                 m.weight = nn.init.xavier_normal(m.weight)
 
     def forward(self, x):
-        x_ = x = F.relu(self.conv1_bn(self.conv1(x)))
+        residual = x
+        x = F.relu(self.conv1_bn(self.conv1(x)))
         x = F.relu(self.conv2_bn(self.conv2(x)))
-        x = F.relu(self.conv3_bn(self.conv3(x))) + x_
-        x = F.max_pool2d(x, 2, stride=2, padding=0)
-        x_ = x = F.relu(self.conv4_bn(self.conv4(x)))
-        x = F.relu(self.conv5_bn(self.conv5(x)))
-        x = F.relu(self.conv6_bn(self.conv6(x))) + x_
-        x = F.max_pool2d(x, 2, stride=2, padding=0)
+        x = self.conv3_bn(self.conv3(x))
+
+        if self.dim_up:
+            x = x + self.dim_up_bn(self.dim_up_conv(residual))
+        else:
+            x = x + residual
+
+        return F.relu(x)
+
+
+class Net(nn.Module):
+    def __init__(self, input_channel=3):
+        super().__init__()
+        self.conv1 = nn.Conv2d(input_channel, 64, 7, padding=1, stride=2)
+        self.conv1_bn = nn.BatchNorm2d(64)
+        # self.dim_up1 = nn.Conv2d(64, 256, 1, padding=0, stride=1)
+        self.res1 = BottleneckBlock(64, 64, dim_up=True)
+        self.res2 = BottleneckBlock(256, 64)
+        self.res3 = BottleneckBlock(256, 64)
+        # self.dim_up2 = nn.Conv2d(256, 512, 1, padding=0, stride=2)
+        self.res4 = BottleneckBlock(256, 128, downsample=True, dim_up=True)
+        self.res5 = BottleneckBlock(512, 128)
+        self.res6 = BottleneckBlock(512, 128)
+        self.res7 = BottleneckBlock(512, 128)
+        # self.dim_up3 = nn.Conv2d(512, 1024, 1, padding=0, stride=2)
+        self.res8 = BottleneckBlock(512, 256, downsample=True, dim_up=True)
+        self.res9 = BottleneckBlock(1024, 256)
+        self.res10 = BottleneckBlock(1024, 256)
+        self.res11 = BottleneckBlock(1024, 256)
+        self.res12 = BottleneckBlock(1024, 256)
+        self.res13 = BottleneckBlock(1024, 256)
+
+        self.fc = nn.Linear(5 * 5 * 1024, 2)
+
+        self.initialize_weights()
+
+    def initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, BottleneckBlock):
+                m.initialize_weights()
+            elif isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
+                m.weight = nn.init.xavier_normal(m.weight)
+
+    def forward(self, x):
+        x = F.relu(self.conv1_bn(self.conv1(x)))
+        x = F.max_pool2d(x, 3, stride=2, padding=1)
+        x = self.res1(x)
+        x = self.res2(x)
+        x = self.res3(x)
+        x = self.res4(x)
+        x = self.res5(x)
+        x = self.res6(x)
+        x = self.res7(x)
+        x = self.res8(x)
+        x = self.res9(x)
+        x = self.res10(x)
+        x = self.res11(x)
+        x = self.res12(x)
+        x = self.res13(x)
+        x = F.avg_pool2d(x, 3, stride=1, padding=1)
         x = x.view(x.shape[0], -1)
-        x = F.relu(self.fc1_bn(self.fc1(x)))
-        x = F.relu(self.fc2_bn(self.fc2(x)))
-        x = self.fc3(x)
+        x = self.fc(x)
 
         return x
--- train.py
+++ train.py
@@ -1,10 +1,21 @@
-import sys; sys.path.append('..')
+# coding: utf-8
+
+# Training
+# ===
+
+# In[1]:
+
+
+import sys;
+
+sys.path.append('..')
 import time
 from math import ceil, floor
 from os.path import join as opj, dirname
 
 import tqdm
 import torch
+import numpy as np
 from torch.utils.data.dataloader import DataLoader
 from torch.autograd import Variable
 import torchvision.transforms as T
@@ -16,23 +27,34 @@ from src.tensorboard_logger import Logger
 from src.utils import mkdir_r
 import src.torchsample.transforms as TST
 
-
 # ## Define Const
-train_data_path = '/home/rlan/datasets/statoil-iceberg/train.json'
+
+# In[2]:
+
+
+train_data_path = '/home/rlan/datasets/statoil-iceberg/trainval_train.json'
+val_data_path = '/home/rlan/datasets/statoil-iceberg/trainval_val.json'
 
 BASE_DIR = '/home/rlan/projects/kaggle/kaggle-statoil-iceberg'
 LOG_DIR = opj(BASE_DIR, 'log')
 CHECKPOINTS_PATH = opj(BASE_DIR, 'checkpoints')
-MAX_EPOCH = 200
-BATCH_SIZE = 256
+MAX_EPOCH = 120
+BATCH_SIZE = 128
 
 # ## Setup Logger
+
+# In[3]:
+
+
 model_id = str(int(time.time()))
 print('model_id: %s' % model_id)
 tb_logger = Logger(opj(LOG_DIR, model_id))
 
-
 # ## Transform
+
+# In[4]:
+
+
 transform = T.Compose([T.ToTensor(),
                        T.Lambda(lambda x: (x - x.min()) / (x.max() - x.min())),
                        T.ToPILImage(),
@@ -47,28 +69,47 @@ transform = T.Compose([T.ToTensor(),
                        T.ToTensor(),
                        T.Lambda(lambda x: x - 0.5)])
 
+val_transform = T.Compose([T.ToTensor(),
+                           T.Lambda(lambda x: (x - x.min()) / (x.max() - x.min()) - 0.5)])
 
 # ## Dataset
+
+# In[5]:
+
+
 dataset = StatoilIcebergDataset(train_data_path, transform=transform)
 loader = DataLoader(dataset, shuffle=True, batch_size=BATCH_SIZE, num_workers=8)
 
+valset = StatoilIcebergDataset(val_data_path, transform=val_transform)
+val_loader = DataLoader(valset, shuffle=False, batch_size=BATCH_SIZE, num_workers=8)
 
 # ## Network
+
+# In[6]:
+
+
 net = Net(input_channel=2).cuda() if torch.cuda.is_available() else Net(input_channel=2)
 net.train()
 
-
 # ## Loss and Optimizer
+
+# In[7]:
+
+
 optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
-lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=80, gamma=0.1)
+lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=10)
 loss_fn = torch.nn.CrossEntropyLoss()
 
-
 # ## Train
-niter_per_epoch = ceil(len(dataset) / BATCH_SIZE)
+
+# In[8]:
+
+
+# niter_per_epoch = ceil(len(dataset) / BATCH_SIZE)
 # pbar = tqdm.tqdm(range(niter_per_epoch * MAX_EPOCH))
 for epoch in range(MAX_EPOCH):
-    lr_scheduler.step()
+    net.train()
+    train_losses = []
     for i_batch, sampled_batch in enumerate(loader):
         data, target = sampled_batch
 
@@ -80,19 +121,43 @@ for epoch in range(MAX_EPOCH):
         optimizer.zero_grad()
         pred = net(data)
         loss = loss_fn(pred, target)
+        train_losses.append(loss.data[0])
         loss.backward()
         optimizer.step()
-        logger.info('Epoch: {:d}, Training loss: {:.4f}'.format(epoch, loss.data[0]))
-        tb_logger.scalar_summary('loss', loss.data[0], epoch * niter_per_epoch + i_batch + 1)
+        logger.info('[Epoch: {:d}, lr: {}], Training loss: {:.4f}'.format(epoch,
+                                                                    optimizer.param_groups[-1]['lr'],
+                                                                    loss.data[0]))
+
+    tb_logger.scalar_summary('train_loss', loss.data[0], epoch + 1)
+
+    net.eval()
+    val_losses = []
+    for i_batch, sampled_batch in enumerate(val_loader):
+        data, target = sampled_batch
+
+        if torch.cuda.is_available():
+            data, target = Variable(data).cuda(), Variable(target).cuda()
+        else:
+            data, target = Variable(data), Variable(target)
+
+        pred = net(data)
+        loss = loss_fn(pred, target)
+        val_losses.append(loss.data[0])
+
+    logger.info('Epoch: {:d}, Validation Loss: {:.4f}'.format(epoch, np.mean(val_losses)))
+    tb_logger.scalar_summary('val_loss', np.mean(val_losses), epoch + 1)
+
+    lr_scheduler.step(np.mean(val_losses))
 
     # (2) Log values and gradients of the parameters (histogram)
     for tag, value in net.named_parameters():
         tag = tag.replace('.', '/')
         tb_logger.histo_summary(tag, value.data.cpu().numpy(), epoch + 1)
-        tb_logger.histo_summary(tag+'/grad', value.grad.data.cpu().numpy(), epoch + 1)
+        tb_logger.histo_summary(tag + '/grad', value.grad.data.cpu().numpy(), epoch + 1)
 
-    if (epoch + 1) % 100 == 0:
+    if (epoch + 1) % 40 == 0:
         cp_path = opj(CHECKPOINTS_PATH, model_id, 'model_%s' % epoch)
         mkdir_r(dirname(cp_path))
         torch.save(net.state_dict(), cp_path)
+
 
